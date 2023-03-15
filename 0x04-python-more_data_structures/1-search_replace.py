8a87379138ad3c5bc655c723d5d4088c86daef15
ThomasOselu <thomasoselu1@gmail.com>
2023-03-15 03:31:58 -0700

    sets

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,6 @@
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for i in range(len(new_list)):
-            my_list[my_list.index(search)] = replace
-            return (new_list)
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
