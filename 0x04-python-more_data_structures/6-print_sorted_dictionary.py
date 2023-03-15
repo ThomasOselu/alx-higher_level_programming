abda46162a57844c6917eaa6ab641b4daf7bc522
ThomasOselu <thomasoselu1@gmail.com>
2023-03-15 08:54:34 -0700

    data strucucture

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
+    [print(f"{key}: {a_dictionary[key]}") for key in sorted(a_dictionary)]
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dictionary = list(map(lambda x: x * 2, a_dictionary))
+    return (new_dictionary)
