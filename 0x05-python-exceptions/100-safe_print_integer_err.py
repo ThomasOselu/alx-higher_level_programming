17b6e3e65dc856795f3b797452300905f99a8292
ThomasOselu <thomasoselu1@gmail.com>
2023-03-27 01:50:53 -0700

    python exception tasks

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    rom sys import stderr
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=stderr)
+        return False
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,6 @@
+0x05. Python - Exceptions
+Resources
+Read or watch:
+
+Errors and Exceptions
+Learn to Program 11 Static & Exception Handling (starting at minute 7)
