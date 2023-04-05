e91b39345f45e28aeb0ca6a11772e9ab0bc096f2
ThomasOselu <thomasoselu1@gmail.com>
2023-04-05 10:29:05 -0700

    python-everything_is_object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a class LockedClass with no class or object attribute"""
+class LockedClass:
+    """a class LockedClass with no class or object attribute,
+    that prevents the user from dynamically creating 
+    new instance attributes, except if the new instance attribute is called first_name
+    """
+    __slots__ = ["first_name"]
