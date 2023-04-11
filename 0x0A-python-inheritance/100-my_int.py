60c1654274edb34d6f23dab7ee9d8b251a7da2f8
ThomasOselu <thomasoselu1@gmail.com>
2023-04-11 10:45:02 -0700

    python inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,10 +4,10 @@
 
 class MyInt(int):
     """representation of class Myint inheriting form int"""
-    def __ne__(self, value):
-        """Overrides != operator with == behavior."""
-        return super().__ne__(value)
-
     def __eq__(self, value):
         """overrides == operator with != behaviours"""
-        return super().__eq__(value)
+        return self.real != value
+
+    def __ne__(self, value):
+        """Overrides != operator with == behavior."""
+        return self.real == value
