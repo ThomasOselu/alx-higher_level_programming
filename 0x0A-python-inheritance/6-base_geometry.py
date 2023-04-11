f0fbbf32589ea73c7c02cfe1d37460e5c7d5e016
ThomasOselu <thomasoselu1@gmail.com>
2023-04-11 05:24:55 -0700

    python inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a basegeomtry class"""
+
+
+class BaseGeometry:
+    """a class representing basegeometry"""
+
+    def area(self):
+        """a function intialization for area"""
+        raise Exception("area() is not implemented")
---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a basegeomtry class"""
+
+
+class BaseGeometry:
+    """a class representing basegeometry"""
+
+    def area(self):
+        """a function intialization for area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """a public method that validates value
+        args:
+            name: a string
+            value: the parameter to validate
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
