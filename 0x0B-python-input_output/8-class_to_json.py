cf43c7ec916659e8917f397e9e5137141c341533
ThomasOselu <thomasoselu1@gmail.com>
2023-04-12 04:20:05 -0700

    python inheritance

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+returns the dictionary description with simple data structure
+"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """
+    returns the dictionary description with simple data structure
+    """
+    return obj.__dict__
