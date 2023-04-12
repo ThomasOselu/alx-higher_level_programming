2a112edba1905523de81c9b89679f8c347e28e05
ThomasOselu <thomasoselu1@gmail.com>
2023-04-12 04:51:51 -0700

    python inheritance

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+a class Student that defines a student
+"""
+
+
+class Student:
+    """
+    a class that defines a student by first_name,last_name,age
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialization of the student attributes
+        args:
+            first_name: the first name of student
+            last_name: the last name of the student
+            age: the age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a attributes representation of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return  {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
