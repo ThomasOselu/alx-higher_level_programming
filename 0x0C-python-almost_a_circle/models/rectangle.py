6ad804d041d8ef7b6a30463dbf99e2d5146107fd
ThomasOselu <thomasoselu1@gmail.com>
2023-04-15 05:57:53 -0700

    python almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,15 +17,15 @@ class Rectangle(Base):
             y=0: position
             id=None: id
         """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
     def height(self):
-        """the gettter for height""" 
+        """the gettter for height"""
         return self.__height
 
     @height.setter
@@ -78,3 +78,72 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """returns the area value of rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """prints in stdout the Rectangle instance
+        with the character #
+        """
+        if self.__width == 0 and self.__height == 0:
+            return ""
+        for y in range(0, self.__y):
+            print()
+        for i in range(1, self.__height+1):
+            for x in range(0, self.__x):
+                print(" ", end="")
+            for j in range(1, self.__width+1):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """returns a string"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        """assigns arguments to each attribute
+        args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents width attribute
+                - 3rd argument represent height attribute
+                - 4th argument represents x attribute
+                - 5th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            b = 0
+            for arg in args:
+                if b == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif b == 1:
+                    self.width = arg
+                elif b == 2:
+                    self.height = arg
+                elif b == 3:
+                    self.x = arg
+                elif b == 4:
+                    self.y = arg
+                b += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
