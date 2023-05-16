99612d36215fdd891f5040b8335e518907110e8e
ThomasOselu <thomasoselu1@gmail.com>
2023-05-16 11:53:04 -0700

    sql_introduction

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL
+-- Results should display both the score and the name (in this order)
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
+-- database name will be passed as an argument of the mysql command
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
