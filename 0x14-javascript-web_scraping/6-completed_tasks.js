4c554abfac10faf54215257ee3685cd9207b027c
ThomasOselu <thomasoselu1@gmail.com>
2023-07-31 14:22:43 -0700

    javascripting web scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,30 @@
 #!/usr/bin/node
-const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
+
+// computes the number of tasks completed by user id; parse data:
+// API URL: https://jsonplaceholder.typicode.com/todos
+
+const rq = require('request');
+
+const apiUrl = process.argv[2];
+
+rq(apiUrl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
     const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
+    const completedTasks = {};
+
+    for (const todo of todos) {
+      if (todo.completed) {
+        const userId = todo.userId;
+        if (!completedTasks[userId]) {
+          completedTasks[userId] = 0;
+        }
+
+        completedTasks[userId]++;
       }
-    });
-    console.log(completed);
+    }
+
+    console.log(completedTasks);
   }
 });
